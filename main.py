9bd1dfca8ddae849a0182249cbf51a8164efddbb
Rodrigo <rodrigo.plassmann@hotmail.com>
2024-08-20 15:42:36 -0300

    loop, função para operações e função para cartesiano

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 #Rodrigo Schiavinatto Plassmann
-def ler_arquivo(nome_arquivo):
-    with open(nome_arquivo, "r") as conjuntos:
+def ler_arquivo(txt):
+    with open(txt, "r") as conjuntos:
         linhas = conjuntos.readlines()
-        for linha in linhas:
-            print(linha.strip())
+    
+    qtde_operacoes = int(linhas[0].strip())
 
-ler_arquivo("tresconjuntos.txt")
+    i = 1
+    for _ in range(qtde_operacoes):
+        operacao = linhas[i].strip()
+        conj1 = set(linhas[i + 1].strip().split(','))
+        conj2 = set(linhas[i + 2].strip().split(','))
+        i += 3
+
+def realizar_operacoes(operacao, conj1, conj2):
+    if operacao == 'U':
+        return conj1.union(conj2)
+    elif operacao == 'D':
+        return conj1.difference(conj2)
+    elif operacao == 'I':
+        return conj1.intersection(conj2)
+    elif operacao == 'C':
+        return cart(conj1, conj2)
+
+def cart(conj1, conj2):
+    pares_ordenados = []
+    for i in range(conj1):
+        for j in range(conj2):
+            par_ordenado = (i, j)
+    pares_ordenados.append(par_ordenado)
+    return pares_ordenados
+
+ler_arquivo("quatroconjuntos.txt")
