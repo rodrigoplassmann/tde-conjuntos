646bc2b08e4c506e3ee295799741270882711ae1
Rodrigo <rodrigo.plassmann@hotmail.com>
2024-08-20 09:07:47 -0300

    ,

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #Rodrigo Schiavinatto Plassmann
+def operacoes(operacao, c1, c2):
+    if operacao == 'U':
+        return c1.union(c2)
+    elif operacao == "I":
+        return c1.union(c2)
+    elif operacao
+
+
+
 def ler_arquivo(nome_arquivo):
     with open(nome_arquivo, "r") as conjuntos:
         linhas = conjuntos.readlines()
-        for linha in linhas:
-            print(linha.strip())
-
+        i = 0
+        while i < len(linhas):
+            operacao = linhas[i].strip()
 ler_arquivo("tresconjuntos.txt")
